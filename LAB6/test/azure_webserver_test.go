8477b133d8bff4aaf74858ad0fa0e6f66f9222c9
Diniz Martins <rodr0304@algonquinlive.com>
2026-07-08 22:27:24 -0400

    Add LAB6 files from laptop

--- LAB6/test/azure_webserver_test.go
+++ LAB6/test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "7c0d13e5-0d38-4a95-8775-910f0da1c263"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "rodr0304",
 		},
 	}
 
