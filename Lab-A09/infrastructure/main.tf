35e426cd60569aca72a33e3c5cef89b20335746c
Diniz Martins <rodr0304@algonquinlive.com>
2026-07-11 20:45:37 -0400

    Testing Husky

--- Lab-A09/infrastructure/main.tf
+++ Lab-A09/infrastructure/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+  name=var.resource_group_name
+  location=var.location
 
   tags = {
     Environment = "Development"
